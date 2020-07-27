4c17ffd3a54bbda3cc844b27a00336341702f4c8
Scott Guthart <seguthart@gmail.com>
2020-07-27 13:50:15 -0400

    refactored impute method

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -4,13 +4,20 @@ sheet = jexcel(document.getElementById('spreadsheet'), {
   wordWrap:true,
 });
 
-const request_impute = async () => {
+const request_impute = async (data) => {
   var xhr = new XMLHttpRequest();
   xhr.open("POST", '/request_impute', true);
+  xhr.onreadystatechange = function() { // Call a function when the state changes.
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        // Request finished. Do processing here.
+        console.log(this.response)
+    }
+}
 
-  xhr.send(JSON.stringify(sheet.getData()));
+  xhr.send(JSON.stringify(data));
 }
 const imputeButton = document.getElementById('imputeButton')
 imputeButton.onclick = () => {
-  request_impute();
+  var data = sheet.getData();
+  request_impute(data);
 }
