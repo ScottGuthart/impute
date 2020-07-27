447b110fc7e57c6d104b51cecf94eb9641bd6444
Scott Guthart <seguthart@gmail.com>
2020-07-27 13:39:33 -0400

    setup post from jexcel to flask

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,10 @@
 import os
-from flask import render_template
+from flask import render_template, request
 from flask_login import login_required
 from app.main import bp
 from app.main.save_user_file import utc_to_human_readable
 from app.auth.confirm import check_confirmed
+import json
 
 
 @bp.route('/')
@@ -12,6 +13,15 @@ from app.auth.confirm import check_confirmed
 @check_confirmed
 def index():
     return render_template('index.html')
+
+
+@bp.route('/request_impute', methods=['GET', 'POST'])
+@login_required
+@check_confirmed
+def request_impute():
+    data = json.loads(request.data)
+    print(data)
+    return 'hi'
 
 
 @bp.app_template_filter("autoversion")
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,5 +1,16 @@
 data = [[,]];
-
-jexcel(document.getElementById('spreadsheet'), {
+sheet = jexcel(document.getElementById('spreadsheet'), {
   minDimensions:[2,2],
+  wordWrap:true,
 });
+
+const request_impute = async () => {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", '/request_impute', true);
+
+  xhr.send(JSON.stringify(sheet.getData()));
+}
+const imputeButton = document.getElementById('imputeButton')
+imputeButton.onclick = () => {
+  request_impute();
+}
